6993dffd1ed92e9b3759ea0bdd24ee7aa17f613a
Maharshi <Maharshi11patel@gmail.com>
2022-07-19 15:29:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/session', (req, res) => {
 let nums = [1, 2, 3, 4, 5, 6, 7, 8];
  
 console.log(_.nth(nums, 3));
-console.log(_.nth(nums, -3));
+
 
 console.log(mathjs.random(1,1000000000000));
 
